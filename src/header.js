23ffc545ef8fdceb01c8af2f00a8f7562b2dbcb4
NODEBOY <kingindoslayer@gmail.com>
2024-11-13 00:08:16 +0700

    Update header.js

--- src/header.js
+++ src/header.js
@@ -1,16 +1,1 @@
-const headers = {
-    "Accept": "application/json, text/plain, */*",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "en",
-    "Content-Type": "multipart/form-data",
-    "Origin": "https://app.bums.bot",
-    "Referer": "https://app.bums.bot/",
-    "Sec-Ch-Ua": '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
-    "Sec-Ch-Ua-Mobile": "?1",
-    "Sec-Ch-Ua-Platform": '"Android"',
-    "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Site": "same-site",
-    "User-Agent": "Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"
-};
-module.exports = headers;
+const _0x2ff2df=_0x11da;(function(_0x425d15,_0x5cd4e6){const _0x135e7d=_0x11da,_0x487da1=_0x425d15();while(!![]){try{const _0x50e26d=-parseInt(_0x135e7d(0x72))/0x1+-parseInt(_0x135e7d(0x74))/0x2+-parseInt(_0x135e7d(0x6f))/0x3+-parseInt(_0x135e7d(0x7e))/0x4*(parseInt(_0x135e7d(0x7b))/0x5)+parseInt(_0x135e7d(0x71))/0x6+-parseInt(_0x135e7d(0x7c))/0x7+parseInt(_0x135e7d(0x7d))/0x8;if(_0x50e26d===_0x5cd4e6)break;else _0x487da1['push'](_0x487da1['shift']());}catch(_0x729325){_0x487da1['push'](_0x487da1['shift']());}}}(_0xde45,0x9f908));const headers={'Accept':_0x2ff2df(0x78),'Accept-Encoding':_0x2ff2df(0x6e),'Accept-Language':'en','Content-Type':_0x2ff2df(0x73),'Origin':_0x2ff2df(0x77),'Referer':'https://app.bums.bot/','Sec-Ch-Ua':_0x2ff2df(0x76),'Sec-Ch-Ua-Mobile':'?1','Sec-Ch-Ua-Platform':_0x2ff2df(0x7a),'Sec-Fetch-Dest':_0x2ff2df(0x70),'Sec-Fetch-Mode':_0x2ff2df(0x79),'Sec-Fetch-Site':'same-site','User-Agent':_0x2ff2df(0x75)};function _0xde45(){const _0x310fb2=['520vIUzRS','5837104ITWKMy','23479112xvyrCU','32612fNpMgE','gzip,\x20deflate,\x20br','155712NMHcqR','empty','6544836dzzTge','774265FrSXPH','multipart/form-data','1728332ozkeJj','Mozilla/5.0\x20(Linux;\x20Android\x2011;\x20Pixel\x205)\x20AppleWebKit/537.36\x20(KHTML,\x20like\x20Gecko)\x20Chrome/115.0.0.0\x20Mobile\x20Safari/537.36','\x22Not/A)Brand\x22;v=\x2299\x22,\x20\x22Google\x20Chrome\x22;v=\x22115\x22,\x20\x22Chromium\x22;v=\x22115\x22','https://app.bums.bot','application/json,\x20text/plain,\x20*/*','cors','\x22Android\x22'];_0xde45=function(){return _0x310fb2;};return _0xde45();}function _0x11da(_0x140922,_0x279adc){const _0xde454f=_0xde45();return _0x11da=function(_0x11da26,_0x793a31){_0x11da26=_0x11da26-0x6e;let _0xc95275=_0xde454f[_0x11da26];return _0xc95275;},_0x11da(_0x140922,_0x279adc);}module['exports']=headers;
